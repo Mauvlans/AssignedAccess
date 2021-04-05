04bda82572662e84e66d69700c370c23e648cb64
Adam <40191591+Mauvlans@users.noreply.github.com>
2021-04-05 12:15:41 -0700

    Update Configure-AA-WMIBridge-XML.ps1

--- SCCM Application/Configure-AA-WMIBridge-XML.ps1
+++ SCCM Application/Configure-AA-WMIBridge-XML.ps1
@@ -1,6 +1,14 @@
-﻿function EnableKiosk {
+function EnableKiosk {
 
-$nameSpaceName="root\cimv2\mdm\dmmap"$className="MDM_AssignedAccess"$obj = Get-CimInstance -Namespace $namespaceName -ClassName $className$obj.Configuration = Get-Content -Path "lockdownprofile.xml" -Raw$obj.Configuration = $obj.Configuration -replace "<", "&lt;"$obj.Configuration = $obj.Configuration -replace ">", "&gt;"Set-CimInstance -CimInstance $obj
+$nameSpaceName="root\cimv2\mdm\dmmap"
+$className="MDM_AssignedAccess"
+$obj = Get-CimInstance -Namespace $namespaceName -ClassName $className
+$obj.Configuration = Get-Content -Path "lockdownprofile.xml" -Raw
+
+$obj.Configuration = $obj.Configuration -replace "<", "&lt;"
+$obj.Configuration = $obj.Configuration -replace ">", "&gt;"
+
+Set-CimInstance -CimInstance $obj
 
 }
 
